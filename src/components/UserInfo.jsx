cbc921d553cfefcc1e9da567e9047bf0872089d2
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-05-04 19:37:42 -0400

    Added Toggle for Personal Section

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -10,39 +10,52 @@ const UserInfo = ({
   onEmailChange,
   onPhoneChange,
 }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div className="user-info-container">
       <h1>Personal Section: </h1>
-      <div className="">
-        <h3>Enter Name: </h3>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter Name:"
-          value={nameInput}
-          onChange={(e) => onNameChange(e.target.value)}
-        />
-      </div>
-      <div>
-        <h3>Enter Email:</h3>
-        <input
-          type="text"
-          name="email"
-          placeholder="Enter Email:"
-          value={emailInput}
-          onChange={(e) => onEmailChange(e.target.value)}
-        />
-      </div>
-      <div>
-        <h3>Enter Phone: </h3>
-        <input
-          type="text"
-          name="phone"
-          placeholder="Enter Phone:"
-          value={phoneInput}
-          onChange={(e) => onPhoneChange(e.target.value)}
-        />
-      </div>
+      <button className="show-btn" onClick={toggleVisibility}>
+        {isActive ? "Hide" : "Show"}
+      </button>
+      {isActive && (
+        <div>
+          <div className="">
+            <h3>Enter Name: </h3>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Name:"
+              value={nameInput}
+              onChange={(e) => onNameChange(e.target.value)}
+            />
+          </div>
+          <div>
+            <h3>Enter Email:</h3>
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter Email:"
+              value={emailInput}
+              onChange={(e) => onEmailChange(e.target.value)}
+            />
+          </div>
+          <div>
+            <h3>Enter Phone: </h3>
+            <input
+              type="text"
+              name="phone"
+              placeholder="Enter Phone:"
+              value={phoneInput}
+              onChange={(e) => onPhoneChange(e.target.value)}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
