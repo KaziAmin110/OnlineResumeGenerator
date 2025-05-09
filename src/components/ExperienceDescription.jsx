7946eff4068eb9388a004a09ec217edecf8e1e50
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-05-09 15:59:12 -0400

    Added Editable DescriptionInput

--- src/components/ExperienceDescription.jsx
+++ src/components/ExperienceDescription.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ExperienceDescription = ({
   descriptions,
@@ -6,8 +6,11 @@ const ExperienceDescription = ({
   description,
   index,
 }) => {
-  const handleChangeDescription = (e) => {
-    onUpdate(e.target.value);
+  const [isEditable, setIsEditable] = useState(true);
+  const [descriptionInput, setDescriptionInput] = useState(description);
+
+  const handleChangeDescriptionInput = (e) => {
+    setDescriptionInput(e.target.value);
   };
 
   const filterRemovedDescription = () => {
@@ -20,9 +23,29 @@ const ExperienceDescription = ({
     onUpdateDescriptions(filterRemovedDescription);
   };
 
+  const handleOnEdit = () => {
+    setIsEditable(!isEditable);
+  };
+
+  const handleOnSave = () => {
+    setIsEditable(!isEditable);
+  };
+
   return (
     <div>
-      <input onChange={() => {}} value={description}></input>
+      {isEditable ? (
+        <p>{description}</p>
+      ) : (
+        <input
+          value={descriptionInput}
+          onChange={handleChangeDescriptionInput}
+        ></input>
+      )}
+      {isEditable ? (
+        <button onClick={handleOnEdit}>Edit</button>
+      ) : (
+        <button onClick={handleOnSave}>Save</button>
+      )}
       <button onClick={handleRemoveDescription}>Remove</button>
     </div>
   );
