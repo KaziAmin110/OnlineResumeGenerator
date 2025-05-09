e7b958f42de62857320203aa62c0cf26cd81cd92
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-05-09 09:40:58 -0400

    Fixed Display Issue

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 const Display = ({ personalInfo, experiences }) => {
+  const { name, email, phone, uniName, uniGpa } = personalInfo;
   return (
     <div>
       <h1>Resume</h1>
       <h2>{name}</h2>
       <h2>{email}</h2>
       <h2>{phone}</h2>
-      <h2>{university}</h2>
-      <h2>{gpa}</h2>
+      <h2>{uniName}</h2>
+      <h2>{uniGpa}</h2>
       {experiences.map((experience, index) => {
         return <h2 key={index}>{experience.job_title}</h2>;
       })}
