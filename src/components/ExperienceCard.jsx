c33e941f74a21733747a57c0efe73857d8a3e87b
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-05-09 09:57:08 -0400

    Added Base Functionality for ExperienceCard

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -7,54 +7,93 @@ const ExperienceCard = ({ experiences, onUpdateExperiences }) => {
   const [companyName, setCompanyName] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
-  const [descriptions, setDescriptions] = useState("");
+  const [descriptions, setDescriptions] = useState([]);
+
+  const handleChangeJobTitle = (e) => {
+    setJobTitle(e.target.value);
+  };
+
+  const handleChangeCompanyName = (e) => {
+    setCompanyName(e.target.value);
+  };
+
+  const handleChangeStartDate = (e) => {
+    setStartDate(e.target.value);
+  };
+
+  const handleChangeEndDate = (e) => {
+    setEndDate(e.target.value);
+  };
+
+  const handleChangeDescriptionInput = (e) => {
+    setDescriptionInput(e.target.value);
+  };
+
+  const handleAddDescription = () => {
+    setDescriptions(...descriptions, descriptionInput);
+  };
+
+  const handleAddExperience = () => {
+    onUpdateExperiences(...experiences, {
+      jobTitle,
+      companyName,
+      startDate,
+      endDate,
+      descriptions,
+    });
+  };
 
   return (
     <div>
-      <h3>Job Title:</h3>
-      <input
-        placeholder=""
-        onChange={handleChangeJobTitle}
-        value={experience.job_title}
-      ></input>
-      <h3>Company Name:</h3>
-      <input
-        placeholder=""
-        onChange={handleChangeCompanyName}
-        value={experience.company_name}
-      ></input>
-      <h3>Start Date:</h3>
-      <input
-        placeholder=""
-        onChange={handleChangeStartDate}
-        value={experience.start_date}
-      ></input>
-      <h3>End Date:</h3>
-      <input
-        placeholder=""
-        onChange={handleChangeEndDate}
-        value={experience.end_date}
-      ></input>
+      <div>
+        <h3>Job Title:</h3>
+        <input
+          placeholder=""
+          onChange={handleChangeJobTitle}
+          value={jobTitle}
+        ></input>
+      </div>
+
+      <div>
+        <h3>Company Name:</h3>
+        <input
+          placeholder=""
+          onChange={handleChangeCompanyName}
+          value={companyName}
+        ></input>
+      </div>
+
+      <div>
+        <h3>Start Date:</h3>
+        <input
+          placeholder=""
+          onChange={handleChangeStartDate}
+          value={startDate}
+        ></input>
+      </div>
+
+      <div>
+        <h3>End Date:</h3>
+        <input
+          placeholder=""
+          onChange={handleChangeEndDate}
+          value={endDate}
+        ></input>
+      </div>
 
       <h3>Description List:</h3>
       {descriptions.map((description, index) => {
         return <ExperienceDescription key={index} description={description} />;
       })}
+
       <input
         placeholder=""
-        onChange={(e) => {
-          handleChangeDescriptionInput(e.target.value);
-        }}
+        onChange={handleChangeDescriptionInput}
         value={descriptionInput}
       ></input>
 
-      <button onClick={() => handleAddDescription(description)}>
-        Add Description
-      </button>
-
-      <button onClick={() => handleAddExperience(experience)}>
-        Add Experience
-      </button>
+      <button onClick={handleAddDescription}>Add Description</button>
+      <button onClick={handleAddExperience}>Add Experience</button>
     </div>
   );
 };
