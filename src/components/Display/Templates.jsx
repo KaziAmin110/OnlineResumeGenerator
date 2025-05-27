4c67df7679b469272818bf5a22586b433643d6ee
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-05-27 18:41:14 -0400

    Fixed Download Button Layout

--- src/components/Display/Templates.jsx
+++ src/components/Display/Templates.jsx
@@ -9,20 +9,26 @@ const Templates = ({ onUpdateTempIndex, handleDownloadPdf, isLoading }) => {
     <div className="template-container">
       <h2>Templates:</h2>
       <div className="temp-buttons-container">
-        <button className="template-btn" onClick={() => handleUpdateTemplate(1)}>
+        <button
+          className="template-btn"
+          onClick={() => handleUpdateTemplate(1)}
+        >
           Jake's Resume
         </button>
-        <button className="template-btn" onClick={() => handleUpdateTemplate(2)}>
+        <button
+          className="template-btn"
+          onClick={() => handleUpdateTemplate(2)}
+        >
           Modern Resume
         </button>
-        <button
-          className="download-btn"
-          onClick={handleDownloadPdf}
-          disabled={isLoading}
-        >
-          {isLoading ? "Downloading..." : "Download"}
-        </button>
       </div>
+      <button
+        className="download-btn"
+        onClick={handleDownloadPdf}
+        disabled={isLoading}
+      >
+        {isLoading ? "Downloading..." : "Download"}
+      </button>
     </div>
   );
 };
