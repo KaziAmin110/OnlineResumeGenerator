b72b6c35fac0798667f6757a581abec9e5716992
Kazi Amin <126922373+KaziAmin110@users.noreply.github.com>
2025-06-05 19:12:06 -0400

    Removed Unnecessary React Import

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import html2pdf from "html2pdf.js";
 import "./Templates.jsx";
 import Templates from "./Templates.jsx";
